af86dfe229d9baa1dbff151c1879f4315b042973
qiufeihong2018 <15058301288@163.com>
2020-01-30 20:50:32 +0800

    feat(添加爬虫文件)案例为爬取知乎专题:1.获得数据,2.清洗数据

--- doc/spider.py
+++ doc/spider.py
@@ -1,12 +1,12 @@
-# 1.15
 import re
 from urllib import request
 
 class Spider():
     url='https://www.zhihu.com/special/all'
     root_pattern='<div class="SpecialListCard-infos">([\s\S]*?)</div>'
-    title_pattern='<a class="SpecialListCard-title"></a>'
-    number_pattern=''
+    # href里面的随机值
+    title_pattern='<a href="/special/[\s\S]*?" class="SpecialListCard-title" data-za-detail-view-id="5814">([\s\S]*?)</a>'
+    number_pattern='<span>([\s\S]*?)</span>'
     def __fetch_content(self):
         res=request.urlopen(Spider.url)
         htmls=res.read()
@@ -17,13 +17,27 @@ class Spider():
         root_html=re.findall(Spider.root_pattern,htmls)
         anchors=[]
         for html in root_html:
+            dict={'number':0,'title':0}
+            number=re.findall(Spider.number_pattern,html)
+            number=number[1].split('<')[0]
             title=re.findall(Spider.title_pattern,html)
-            number=re.findall()
-        print(root_html[0])
+            title=title[0]
+            dict['number']=number
+            dict['title']=title
+            anchors.append(dict)
+        return anchors
 
+
+    def __sort(self,anchors):
+        result=[]
+        for item in anchors:
+            print(item)
+        # return result
+        
     def go(self):
         htmls= self.__fetch_content()
-        self.__analysis(htmls)
+        anchors=self.__analysis(htmls)
+        result=self.__sort(anchors)
 
 spider=Spider()
 spider.go()
