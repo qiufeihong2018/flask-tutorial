1eea315a081ec34cd6d6d272d6643557848cafce
qiufeihong2018 <15058301288@163.com>
2020-02-02 20:07:04 +0800

    feat(添加词云文件)案例为制作词云:1.其余属性2.改变宽高3.改变形状

--- doc/word_cloud.py
+++ doc/word_cloud.py
@@ -3,20 +3,15 @@ import PIL .Image as image
 import jieba
 import numpy as np
 
-def __translate_zh(text):
+def participle_word(text):
     text_list=jieba.cut(text)
     res=' '.join(text_list)
     return res
 
-with open("D:\\githubMe\\flask-tutorial\\doc\\coronavirus_data.json",'r', encoding='UTF-8') as fp:
-# with open("D:\\githubMe\\flask-tutorial\\doc\\Coronavirus_2.txt",'r', encoding='UTF-8') as fp:
+with open("D:\\githubMe\\flask-tutorial\\doc\\coronavirus_data.txt",'r', encoding='UTF-8') as fp:
     text=fp.read()
-    text=__translate_zh(text)
-    # shade=np.array(image.open('D:\\githubMe\\flask-tutorial\\doc\\Coronavirus9.png'))
-    # wordcloud=WordCloud(mask=shade,font_path="D:\\githubMe\\flask-tutorial\\doc\\msyh.ttc").generate(text)
-    wordcloud=WordCloud(font_path="D:\\githubMe\\flask-tutorial\\doc\\msyh.ttc").generate(text)
+    text=participle_word(text)
+    shade=np.array(image.open('D:\\githubMe\\flask-tutorial\\doc\\wordcloud2.png'))
+    wordcloud=WordCloud(font_path="D:\\githubMe\\flask-tutorial\\doc\\msyh.ttc",width=800,height=800,mask=shade).generate(text)
     word_image=wordcloud.to_image()
-    word_image.save('Coronavirus_test_5.png','png')
-
-
-
+    word_image.save('coronavirus_test_10.png','png')
