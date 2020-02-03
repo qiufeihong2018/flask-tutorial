80aaff01934cd20db49815bd9d23af59aea92dbb
qiufeihong2018 <15058301288@163.com>
2020-02-03 20:13:56 +0800

    docs(python教程)函数案例

---
+++ doc/function.py
@@ -0,0 +1,45 @@
+# # 一元二次方程
+# import math
+
+# def quadratic(a,b,c):
+#     x1=(-b+math.sqrt(b*b-4*a*c))/(2*a)
+#     x2=(-b-math.sqrt(b*b-4*a*c))/(2*a)
+#     return x1,x2
+
+# print(quadratic(2,3,1))
+
+
+# def add_end(L=[]):
+#     L.append('end')
+#     return L
+
+# print(add_end())
+
+# # def add_end(L=None):
+# # ...     if L is None:
+# # ...             L=[]
+# # ...     L.append('end')
+# # ...     return L
+# # ...
+
+# def calc(*numbers):
+#     res=0
+#     for num in numbers:
+#         res=res+num*num
+#     return res
+
+# # print(calc([1,2,3]))
+# # print(calc((1,2,3)))
+# # 在参数前面加上*，numbers就会变成一个元组
+# print(calc(1,2,3))
+
+def product(*numbers):
+    res=1
+    for num in numbers:
+        res=res*num
+    return res
+
+print(product(5))
+print(product(5,6))
+print(product(5,6))
+print(product(5,6,7,9))
