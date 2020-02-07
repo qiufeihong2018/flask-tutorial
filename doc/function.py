72d0c93924d490fdfa54711bd5c9a8cd6a8f14c5
qiufeihong2018 <15058301288@163.com>
2020-02-07 13:43:20 +0800

    docs(python教程)面向对象编程:实例属性和雷属性

--- doc/function.py
+++ doc/function.py
@@ -593,31 +593,68 @@
 # 练习
 # 请把下面的Student对象的gender字段对外隐藏起来，用get_gender()和set_gender()代替，并检查参数有效性：
 
-# -*- coding: utf-8 -*-
+# class Student(object):
+#     def __init__(self, name, gender):
+#         self.name = name
+#         self.__gender = gender
+
+#     def get_gender(self):
+#         return self.__gender
+
+#     def set_gender(self,gender):
+#         if gender!='half-visible femal':
+#             self.__gender=gender
+
+# # 测试:
+# bart = Student('Bart', 'male')
+# if bart.get_gender() != 'male':
+#     print('测试失败!')
+# else:
+#     bart.set_gender('female')
+#     if bart.get_gender() != 'female':
+#         print('测试失败!')
+#     else:
+#         print('测试成功!')
+#     bart.set_gender('half-visible femal')
+#     if bart.get_gender()=='half-visible femal':
+#         print('测试失败!')
+#     else:
+#         print('测试成功!')
+
+# 判断对象
+# import types
+# def a():
+#     pass
+# print(type(a)==types.FunctionType)
+# print(type(abs)==types.BuiltinFunctionType)
+# print(type(lambda x:x)==types.LambdaType)
+# print(type(x for x in range(10))==types.GeneratorType)
+
+
+
+# 练习
+# 为了统计学生人数，可以给Student类增加一个类属性，每创建一个实例，该属性自动增加：
+
 class Student(object):
-    def __init__(self, name, gender):
+    count = 0
+
+    def __init__(self, name):
         self.name = name
-        self.__gender = gender
+        # 不能是self
+        Student.count=Student.count+1
 
-    def get_gender(self):
-        return self.__gender
-
-    def set_gender(self,gender):
-        if gender!='half-visible femal':
-            self.__gender=gender
 
 # 测试:
-bart = Student('Bart', 'male')
-if bart.get_gender() != 'male':
+if Student.count != 0:
     print('测试失败!')
 else:
-    bart.set_gender('female')
-    if bart.get_gender() != 'female':
+    bart = Student('Bart')
+    if Student.count != 1:
         print('测试失败!')
     else:
-        print('测试成功!')
-    bart.set_gender('half-visible femal')
-    if bart.get_gender()=='half-visible femal':
-        print('测试失败!')
-    else:
-        print('测试成功!')
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
