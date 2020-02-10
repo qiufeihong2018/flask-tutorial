555eeb81859c233aaae71e5cf7ec81e2057b1523
qiufeihong2018 <15058301288@163.com>
2020-02-10 20:18:07 +0800

    docs(python教程)错误、调试和测试:单元测试

--- doc/function.py
+++ doc/function.py
@@ -972,3 +972,125 @@
 
 # 这说明`return int(s)`错了。
 # 改成`return float(s)`即可。
+
+
+# 单元测试
+# class Dict(dict):
+#     def __init__(self,**kw):
+#         super().__init__(**kw)
+
+#     def __getattr__(self,key):
+#         try:
+#             return self[key]
+#         except KeyError:
+#             raise AttributeError(r"'Dict'object has no  attribute '%s'="% key)
+
+
+#     def __setattr__(self,key,value):
+#         self[key]=value
+
+# import unittest
+
+# class TestDict(unittest.TestCase):
+#     def setUp(self):
+#         print('setUp...例如：登录')
+
+#     def tearDown(self):
+#         print('tearDown...例如：注销')
+#     def test_init(self):
+#         d=Dict(a=1,b='test')
+#         self.assertEqual(d.a,1)
+#         self.assertEqual(d.b,'test')
+#         self.assertTrue(isinstance(d,dict))
+
+#     def test_key(self):
+#         d=Dict()
+#         d['key']='value'
+#         self.assertEqual(d.key,'value')
+
+#     def test_attr(self):
+#         d = Dict()
+#         d.key = 'value'
+#         self.assertTrue('key' in d)
+#         self.assertEqual(d['key'], 'value')
+
+#     def test_keyerror(self):
+#         d = Dict()
+#         with self.assertRaises(KeyError):
+#             value = d['empty']
+
+#     def test_attrerror(self):
+#         d = Dict()
+#         with self.assertRaises(AttributeError):
+#             value = d.empty
+
+# if __name__=='__main__':
+#     unittest.main()
+
+# # .....
+# # ----------------------------------------------------------------------
+# # Ran 5 tests in 0.001s
+
+# # OK
+
+
+# 练习
+# 对Student类编写单元测试，结果发现测试不通过，请修改Student类，让测试通过：
+# 错误
+# class Student(object):
+#     def __init__(self, name, score):
+#         self.name = name
+#         self.score = score
+#     def get_grade(self):
+#         if self.score >= 60:
+#             return 'B'
+#         if self.score >= 80:
+#             return 'A'
+#         return 'C'
+
+# 正确如下：
+
+# import unittest
+# class Student(object):
+#     def __init__(self, name, score):
+#         self.name = name
+#         self.score = score
+#     def get_grade(self):
+#         if self.score>100 or self.score<0:
+#             raise ValueError
+#         if self.score >= 80:
+#             return 'A'
+#         if self.score >= 60:
+#             return 'B'
+#         return 'C'
+
+# class TestStudent(unittest.TestCase):
+
+#     def test_80_to_100(self):
+#         s1 = Student('Bart', 80)
+#         s2 = Student('Lisa', 100)
+#         self.assertEqual(s1.get_grade(), 'A')
+#         self.assertEqual(s2.get_grade(), 'A')
+
+#     def test_60_to_80(self):
+#         s1 = Student('Bart', 60)
+#         s2 = Student('Lisa', 79)
+#         self.assertEqual(s1.get_grade(), 'B')
+#         self.assertEqual(s2.get_grade(), 'B')
+
+#     def test_0_to_60(self):
+#         s1 = Student('Bart', 0)
+#         s2 = Student('Lisa', 59)
+#         self.assertEqual(s1.get_grade(), 'C')
+#         self.assertEqual(s2.get_grade(), 'C')
+
+#     def test_invalid(self):
+#         s1 = Student('Bart', -1)
+#         s2 = Student('Lisa', 101)
+#         with self.assertRaises(ValueError):
+#             s1.get_grade()
+#         with self.assertRaises(ValueError):
+#             s2.get_grade()
+
+# if __name__ == '__main__':
+#     unittest.main()
