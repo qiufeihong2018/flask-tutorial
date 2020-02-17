d4ca96cc0eb99abbdf92de0050b5fe3bc391c32d
qiufeihong2018 <15058301288@163.com>
2020-02-17 22:10:05 +0800

    docs(python教程)正则表达式：第一题问题

--- doc/function.py
+++ doc/function.py
@@ -1472,3 +1472,19 @@
 # for i in range(multiprocessing.cpu_count()):
 #     t = threading.Thread(target=loop)
 #     t.start()
+
+
+
+# 练习
+# 请尝试写一个验证Email地址的正则表达式。版本一应该可以验证出类似的Email：
+
+import re
+def is_valid_email(addr):
+    return True
+
+# 测试:
+assert is_valid_email('someone@gmail.com')
+assert is_valid_email('bill.gates@microsoft.com')
+assert not is_valid_email('bob#example.com')
+assert not is_valid_email('mr-bob@example.com')
+print('ok')
